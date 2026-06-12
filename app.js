cc509521cc5c0aaf1144475fe2896498c12ff10f
kevinsaenz03 <3380562@clases.edu.sv>
2026-06-12 11:46:59 -0600

    Proyecto de prueba

---
+++ app.js
@@ -0,0 +1,99 @@
+let deferredPrompt = null;
+
+const installButton =
+        document.getElementById("btnInstall");
+
+// Mostrar temporalmente el botón
+installButton.style.display = "inline-block";
+
+// Registrar Service Worker
+if ("serviceWorker" in navigator) {
+
+    window.addEventListener("load", async () => {
+
+        try {
+
+            const registration =
+                    await navigator.serviceWorker.register(
+                            "/service-worker.js"
+                            );
+
+            console.log(
+                    "Service Worker registrado",
+                    registration
+                    );
+
+        } catch (error) {
+
+            console.error(
+                    "Error al registrar Service Worker",
+                    error
+                    );
+
+        }
+
+    });
+
+}
+
+// Evento cuando Chrome considera instalable la PWA
+window.addEventListener(
+        "beforeinstallprompt",
+        (event) => {
+
+    console.log("PWA INSTALABLE");
+
+    event.preventDefault();
+
+    deferredPrompt = event;
+
+    installButton.style.display = "inline-block";
+
+}
+);
+
+// Botón Instalar
+installButton.addEventListener(
+        "click",
+        async () => {
+
+    if (!deferredPrompt) {
+
+        alert(
+                "Chrome aún no considera instalable esta PWA."
+                );
+
+        return;
+
+    }
+
+    deferredPrompt.prompt();
+
+    const result =
+            await deferredPrompt.userChoice;
+
+    console.log(
+            "Resultado instalación:",
+            result.outcome
+            );
+
+    deferredPrompt = null;
+
+    installButton.style.display = "none";
+
+}
+);
+
+// Evento cuando la instalación finaliza
+window.addEventListener(
+        "appinstalled",
+        () => {
+
+    console.log(
+            "PWA instalada correctamente"
+            );
+
+    installButton.style.display = "none";
+
+}
+);
---
+++ service-worker.js
@@ -0,0 +1,69 @@
+const CACHE_NAME = "demo-pwa-v3";
+
+const FILES_TO_CACHE = [
+    "/",
+    "/manifest.webmanifest",
+    "/css/style.css",
+    "/js/app.js",
+    "/icons/icone-internet-bleue.png",
+    "/icons/icone-internet-bleue1.png"
+];
+
+self.addEventListener("install", event => {
+
+    event.waitUntil(
+
+        caches.open(CACHE_NAME)
+            .then(cache => {
+
+                return cache.addAll(FILES_TO_CACHE);
+
+            })
+
+    );
+
+    self.skipWaiting();
+
+});
+
+self.addEventListener("activate", event => {
+
+    event.waitUntil(
+
+        caches.keys()
+            .then(keys => {
+
+                return Promise.all(
+
+                    keys.map(key => {
+
+                        if (key !== CACHE_NAME) {
+                            return caches.delete(key);
+                        }
+
+                    })
+
+                );
+
+            })
+
+    );
+
+    self.clients.claim();
+
+});
+
+self.addEventListener("fetch", event => {
+
+    event.respondWith(
+
+        caches.match(event.request)
+            .then(response => {
+
+                return response || fetch(event.request);
+
+            })
+
+    );
+
+});
